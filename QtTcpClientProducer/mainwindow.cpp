827596b3fc2b85b7c68b6e88064e3297d24a9c40
igorcbrito <igorcbrito96@gmail.com>
2018-11-28 08:23:42 -0300

    arquivos add

---
+++ QtTcpClientProducer/mainwindow.cpp
@@ -0,0 +1,51 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QDateTime>
+#include <string>
+
+using namespace std;
+
+MainWindow::MainWindow(QWidget *parent) :
+  QMainWindow(parent), ui(new Ui::MainWindow){
+  ui->setupUi(this);
+  socket = new QTcpSocket(this);
+  tcpConnect(); 
+
+  connect(ui->pushButtonPut,
+          SIGNAL(clicked(bool)),
+          this,
+          SLOT(connectToHost()));
+}
+
+void MainWindow::tcpConnect(){
+  socket->connectToHost(ip,1234);
+  if(socket->waitForConnected(3000)){
+    qDebug() << "Connected";
+  }
+  else{
+    qDebug() << "Disconnected";
+  }
+}
+
+void MainWindow::putData(){
+  QDateTime datetime;
+  QString str;
+  qint64 msecdate;
+
+  if(socket->state()== QAbstractSocket::ConnectedState){
+
+    msecdate = QDateTime::currentDateTime().toMSecsSinceEpoch();
+    str = "set "+ QString::number(msecdate) + " " + QString::number(qrand()%35)+"\r\n";
+
+      qDebug() << str;
+      qDebug() << socket->write(str.toStdString().c_str()) << " bytes written";
+      if(socket->waitForBytesWritten(3000)){
+        qDebug() << "wrote";
+      }
+  }
+}
+
+MainWindow::~MainWindow(){
+  delete socket;
+  delete ui;
+}
